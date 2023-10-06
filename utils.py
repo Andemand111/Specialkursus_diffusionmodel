ba92e7b8d5b41b6e3c1c98897bfebdb4a686d42e
Andemand111 <Andbagge@gmail.com>
2023-10-06 21:35:21 +0200

    Added loss landscape
    Also improved network, added standard model class, and other small changes

--- utils.py
+++ utils.py
@@ -51,29 +51,33 @@ class NoiseSchedule:
 class Block(nn.Module):
     def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
         super().__init__()
-        self.time_mlp = nn.Sequential(
-            nn.Linear(time_emb_dim, time_emb_dim),
-            nn.ReLU(),
-            nn.Linear(time_emb_dim, out_ch)
-        )
+        self.time_linear = nn.Sequential(nn.Linear(time_emb_dim, time_emb_dim),
+                                            nn.ReLU(),
+                                            nn.Dropout(0.2),
+                                            nn.Linear(time_emb_dim, out_ch))
+
         if up:
             self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
             self.transform = nn.ConvTranspose2d(out_ch, out_ch, 3, padding=1)
         else:
             self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
             self.transform = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+            
         self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
         self.bnorm1 = nn.BatchNorm2d(out_ch)
         self.bnorm2 = nn.BatchNorm2d(out_ch)
         self.relu  = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
         
     def forward(self, x, t, ):
         h = self.bnorm1(self.relu(self.conv1(x)))
-        time_emb = self.relu(self.time_mlp(t))
+        time_emb = self.relu(self.time_linear(t))
         time_emb = time_emb.view(-1, time_emb.shape[1], 1, 1)
         h = h + time_emb
         h = self.bnorm2(self.relu(self.conv2(h)))
+        h = self.dropout(h)
         return self.transform(h)
+
 
 class ResNET(nn.Module):
     def  __init__(self, channels=3, time_emb_dim=64, down_channels = [64, 128, 256, 512, 1024]):
@@ -81,10 +85,10 @@ class ResNET(nn.Module):
 
         up_channels = down_channels[::-1]
 
-        self.time_mlp = nn.Sequential(
-                nn.Linear(time_emb_dim, time_emb_dim),
-                nn.ReLU()
-            )
+        self.time_linear = nn.Sequential(nn.Linear(time_emb_dim, time_emb_dim),
+                                            nn.ReLU(),
+                                            nn.Dropout(0.2),
+                                            nn.Linear(time_emb_dim, time_emb_dim))
         
         self.conv0 = nn.Conv2d(channels, down_channels[0], 3, padding=1)
 
@@ -96,17 +100,18 @@ class ResNET(nn.Module):
                                         time_emb_dim, up=True) \
                     for i in range(len(up_channels)-1)])
         
-        self.output = nn.Sequential(
-            nn.Conv2d(up_channels[-1], channels, 3, padding=1),
-        )
+        self.output = nn.Conv2d(up_channels[-1], channels, 1, padding=0)
 
     def forward(self, x, t):
-        t = self.time_mlp(t)
         x = self.conv0(x)
+
+        t = self.time_linear(t)
         residual_inputs = []
+
         for down in self.downs:
             x = down(x, t)
             residual_inputs.append(x)
+
         for up in self.ups:
             residual_x = residual_inputs.pop()
             x = torch.cat((x, residual_x), dim=1)           
@@ -115,14 +120,6 @@ class ResNET(nn.Module):
         out = self.output(x)
 
         return out
-    
-    def save_model(self, path):
-        torch.save(self.state_dict(), path)
-        print("Model saved to {}".format(path))
-
-    def load_model(self, path):
-        self.load_state_dict(torch.load(path))
-        print("Model loaded from {}".format(path))
 
 class MNIST(Dataset):
     def __init__(self, n = None):
@@ -148,9 +145,10 @@ class CIFAR10(Dataset):
         super().__init__()
 
         data = torchvision.datasets.CIFAR10(root='../data', download=True, transform=transforms.ToTensor())
-        data = data.data.float() / 255
-        data = data.view(-1, 3, 32, 32)
+        data = torch.from_numpy(data.data)
+        data = data.permute(0,3,1,2).float() / 255
         self.data = data * 2 - 1
+        self.n = n
 
     def __len__(self):
         if self.n is None:
@@ -160,23 +158,57 @@ class CIFAR10(Dataset):
     
     def __getitem__(self, idx):
         return self.data[idx]
-
-class SimpleModel(nn.Module):
-    def __init__(self, network, train_loader, noise_schedule, dimensions, device):
+    
+class Model(nn.Module):
+    def __init__(self, network, noise_schedule, dimensions, device):
         super().__init__()
-        self.network = network
-        self.train_loader = train_loader
+        self.network = network.to(device)   
         self.noise_schedule = noise_schedule
         self.dimensions = dimensions
         self.img_size = torch.prod(torch.tensor(dimensions))
         self.device = device
+    
+    def sample(self):
+        with torch.no_grad():
+            xt = torch.randn((1, self.img_size))
+            print("Sampling image..")
+            for t in tqdm(reversed(range(1, self.noise_schedule.time_steps)), total=self.noise_schedule.time_steps-1):
+                torch.cuda.empty_cache() ## clear memory, otherwise it will crash due to the "big" loop
+                embedding = SinusoidalEmbeddings(torch.tensor(t).view(1,1))
+                
+                xt = xt.to(self.device)
+                embedding = embedding.to(self.device)
+
+                xt = self.get_prior(xt, embedding, t)
+
+            print("Done sampling image")
+
+            return xt
+        
+    def save_model(self):
+        torch.save(self.state_dict(), self.path)
+        print("Model saved to {}".format(self.path))
+    
+    def load_model(self):
+        try:
+            self.load_state_dict(torch.load(self.path))
+            print("Model loaded from {}".format(self.path))
+        except:
+            print("Failed to load model from {}".format(self.path))
+            print("Initializing new model")
+
+
+class SimpleModel(Model):
+    def __init__(self, network, noise_schedule, dimensions, device):
+        super().__init__(network, noise_schedule, dimensions, device)
         self.path = "../models/simple_model.pt"
     
     def loss(self, x0):
-        ts = self.noise_schedule.sample_time_steps(self.train_loader.batch_size)
+        batch_size = len(x0)
+        ts = self.noise_schedule.sample_time_steps(batch_size)
         embeddings = SinusoidalEmbeddings(ts)
         xt, eps, _ = self.noise_schedule.make_noisy_images(x0.flatten(1), ts)
-        xt = xt.view(self.train_loader.batch_size, *self.dimensions)
+        xt = xt.view(batch_size, *self.dimensions)
 
         xt = xt.to(self.device)
         embeddings = embeddings.to(self.device)
@@ -186,59 +218,30 @@ class SimpleModel(nn.Module):
         loss = mse(pred.flatten(1), eps)
         return loss
     
-    def sample(self):
-        with torch.no_grad():
-            xt = torch.randn((1, self.img_size))
-            print("Sampling image..")
-            for t in tqdm(reversed(range(1, self.noise_schedule.time_steps)), total=self.noise_schedule.time_steps-1):
-                torch.cuda.empty_cache() ## clear memory, otherwise it will crash due to the "big" loop
-                embedding = SinusoidalEmbeddings(torch.tensor(t).view(1,1))
-                
-                xt = xt.to(self.device)
-                embedding = embedding.to(self.device)
+    def get_prior(self, xt, embedding, t):
+        eps = self.network(xt.view(1, *self.dimensions), embedding).flatten()
 
-                eps = self.network(xt.view(1, *self.dimensions), embedding).flatten()
+        k1 = 1 / self.noise_schedule.sqrt_alpha[t]
+        k2 = (1 - self.noise_schedule.alpha[t]) / self.noise_schedule.sqrt_one_minus_alpha_hat[t]
 
-                k1 = 1 / self.noise_schedule.sqrt_alpha[t]
-                k2 = (1 - self.noise_schedule.alpha[t]) / self.noise_schedule.sqrt_one_minus_alpha_hat[t]
-
-                xt = k1 * (xt - k2 * eps)
-                if t > 1:
-                    xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
-
-            print("Done sampling image")
-
-            return xt
+        xt = k1 * (xt - k2 * eps)
+        if t > 1:
+            xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
         
-    def save_model(self):
-        torch.save(self.state_dict(), self.path)
-        print("Model saved to {}".format(self.path))
-    
-    def load_model(self):
-        try:
-            self.load_state_dict(torch.load(self.path))
-            print("Model loaded from {}".format(self.path))
-        except:
-            print("Failed to load model from {}".format(self.path))
-            print("Initializing new model")
+        return xt
     
 
-class MuModel(nn.Module):
-    def __init__(self, network, train_loader, noise_schedule, dimensions, device):
-        super().__init__()
-        self.network = network
-        self.train_loader = train_loader
-        self.noise_schedule = noise_schedule
-        self.dimensions = dimensions
-        self.img_size = torch.prod(torch.tensor(dimensions))
-        self.device = device
+class MuModel(Model):
+    def __init__(self, network, noise_schedule, dimensions, device):
+        super().__init__(network, noise_schedule, dimensions, device)
         self.path = "../models/mu_model.pt"
     
     def loss(self, x0):
-        ts = self.noise_schedule.sample_time_steps(self.train_loader.batch_size)
+        batch_size = len(x0)
+        ts = self.noise_schedule.sample_time_steps(batch_size)
         embeddings = SinusoidalEmbeddings(ts)
         xt, _, _ = self.noise_schedule.make_noisy_images(x0.flatten(1), ts)
-        xt = xt.view(self.train_loader.batch_size, *self.dimensions)
+        xt = xt.view(batch_size, *self.dimensions)
 
         xt = xt.to(self.device)
         embeddings = embeddings.to(self.device)
@@ -248,28 +251,16 @@ class MuModel(nn.Module):
         loss = mse(pred.flatten(1), x0.flatten(1))
         return loss
     
-    def sample(self):
-        with torch.no_grad():
-            xt = torch.randn((1, self.img_size))
-            print("Sampling image..")
-            for t in tqdm(reversed(range(1, self.noise_schedule.time_steps)), total=self.noise_schedule.time_steps-1):
-                torch.cuda.empty_cache() ## clear memory, otherwise it will crash due to the "big" loop
-                embedding = SinusoidalEmbeddings(torch.tensor(t).view(1,1))
-                
-                xt = xt.to(self.device)
-                embedding = embedding.to(self.device)
+    def get_prior(self, xt, embedding, t):
+        x0 = self.network(xt.view(1, *self.dimensions), embedding).flatten(1)
+        k1 = self.noise_schedule.sqrt_alpha[t] * (1 - self.noise_schedule.alpha_hat[t - 1]) * xt
+        k2 = self.noise_schedule.sqrt_alpha_hat[t - 1] * (1 - self.noise_schedule.alpha[t]) * x0
+        xt = (k1 + k2) / (1 - self.noise_schedule.alpha_hat[t])
 
-                x0 = self.network(xt.view(1, *self.dimensions), embedding).flatten(1)
-                k1 = self.noise_schedule.sqrt_alpha[t] * (1 - self.noise_schedule.alpha_hat[t - 1]) * xt
-                k2 = self.noise_schedule.sqrt_alpha_hat[t - 1] * (1 - self.noise_schedule.alpha[t]) * x0
-                xt = (k1 + k2) / (1 - self.noise_schedule.alpha_hat[t])
+        if t > 1:
+            xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
 
-                if t > 1:
-                    xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
-
-            print("Done sampling image")
-
-            return xt
+        return xt
         
     def save_model(self):
         torch.save(self.state_dict(), self.path)
@@ -282,3 +273,17 @@ class MuModel(nn.Module):
         except:
             print("Failed to load model from {}".format(self.path))
             print("Initializing new model")
+
+noise_schedule = NoiseSchedule(1e-4, 0.02, 1000)
+dimensions = [3, 32, 32]
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+network = ResNET()
+model = MuModel(network, noise_schedule, dimensions, device)
+dummy_input = torch.randn(1, 3, 32, 32).to(device)
+dummy_time = torch.randn(1, 1)
+dummy_embedding = SinusoidalEmbeddings(dummy_time).to(device)
+dummy_out = model.network(dummy_input, dummy_embedding)
+num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+# print("Number of parameters: {}".format(num_params))
+# print(dummy_out.shape)
+# xt = model.sample()W
