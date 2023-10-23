3996cbe2e6af9eb91996c4ae14e33d534f2a6264
Andemand111 <Andbagge@gmail.com>
2023-10-23 23:03:05 +0200

    Small changes

--- utils.py
+++ utils.py
@@ -34,11 +34,8 @@ class NoiseSchedule:
         self.sqrt_alpha_hat = torch.sqrt(self.alpha_hat)
         self.one_minus_alpha_hat = 1 - self.alpha_hat
         self.sqrt_one_minus_alpha_hat = torch.sqrt(self.one_minus_alpha_hat)
-        """ self.sigma_sq = (1 - self.alpha) * torch.roll(self.one_minus_alpha_hat, 1) / self.one_minus_alpha_hat
+        self.sigma_sq = (1 - self.alpha) * torch.roll(self.one_minus_alpha_hat, 1) / self.one_minus_alpha_hat
         self.sigma_sq[0] = 0
-        self.sigma = torch.sqrt(self.sigma_sq) """
-
-        self.sigma_sq = self.beta
         self.sigma = torch.sqrt(self.sigma_sq)
 
     def sample_time_steps(self, n):
@@ -51,77 +48,6 @@ class NoiseSchedule:
         x_t = mu + std * eps
 
         return x_t, eps, mu
-
-class Block(nn.Module):
-    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
-        super().__init__()
-        self.time_linear = nn.Sequential(nn.Linear(time_emb_dim, time_emb_dim),
-                                            nn.ReLU(),
-                                            nn.Linear(time_emb_dim, out_ch))
-
-        if up:
-            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
-            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 3, padding=1)
-        else:
-            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-            self.transform = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-            
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bnorm1 = nn.BatchNorm2d(out_ch)
-        self.bnorm2 = nn.BatchNorm2d(out_ch)
-        self.relu  = nn.ReLU()
-        self.dropout = nn.Dropout(0.1)
-        
-    def forward(self, x, t, ):
-        h = self.bnorm1(self.relu(self.conv1(x)))
-        time_emb = self.relu(self.time_linear(t))
-        time_emb = time_emb.view(-1, time_emb.shape[1], 1, 1)
-        h = h + time_emb
-        h = self.bnorm2(self.relu(self.conv2(h)))
-        h = self.dropout(h)
-        return self.transform(h)
-
-
-class ResNET(nn.Module):
-    def  __init__(self, channels=3, time_emb_dim=256, down_channels = [64, 128, 256, 512, 1024]):
-        super().__init__()
-
-        up_channels = down_channels[::-1]
-
-        self.time_linear = nn.Sequential(nn.Linear(time_emb_dim, time_emb_dim),
-                                            nn.ReLU(),
-                                            nn.Linear(time_emb_dim, time_emb_dim))
-        
-        self.conv0 = nn.Conv2d(channels, down_channels[0], 3, padding=1)
-
-        self.downs = nn.ModuleList([Block(down_channels[i], down_channels[i+1], \
-                                    time_emb_dim) \
-                    for i in range(len(down_channels)-1)])
-
-        self.ups = nn.ModuleList([Block(up_channels[i], up_channels[i+1], \
-                                        time_emb_dim, up=True) \
-                    for i in range(len(up_channels)-1)])
-        
-        self.output = nn.Conv2d(up_channels[-1], channels, 1, padding=0)
-
-    def forward(self, x, t):
-        x = self.conv0(x)
-
-        t = self.time_linear(t)
-        residual_inputs = []
-
-        for down in self.downs:
-            x = down(x, t)
-            residual_inputs.append(x)
-
-        for up in self.ups:
-            residual_x = residual_inputs.pop()
-            x = torch.cat((x, residual_x), dim=1)           
-            x = up(x, t)
-
-        out = self.output(x)
-
-        return out
 
 class MNIST(Dataset):
     def __init__(self, n = None):
@@ -226,22 +152,21 @@ class Model(nn.Module):
         self.img_size = torch.prod(torch.tensor(dimensions))
         self.device = device
     
-    def sample(self, xt=None, T=None, show_progress=True):
+    def sample(self, xt=None, T=None):
         steps = torch.linspace(0, self.noise_schedule.time_steps, 10, dtype=torch.int)
         progress_list = []
 
         with torch.no_grad():
             xt = torch.randn((1, self.img_size)) if xt is None else xt
             T = self.noise_schedule.time_steps if T is None else T
+            
             print("Sampling image..")
             for t in tqdm(reversed(range(0, T)), total = T - 1):
                 torch.cuda.empty_cache() ## clear memory, otherwise it will crash due to the "big" loop
-                embedding = SinusoidalEmbeddings(torch.tensor(t).view(1,1))
                 
                 xt = xt.to(self.device)
-                embedding = embedding.to(self.device)
 
-                xt = self.get_prior(xt, embedding, t)
+                xt = self.get_prior(xt, t)
                 if t in steps:
                     progress_list.append(xt.view(*self.dimensions).cpu())
 
@@ -277,21 +202,21 @@ class SimpleModel(Model):
         batch_size = len(x0)
 
         ts = self.noise_schedule.sample_time_steps(batch_size) if ts is None else ts
-        embeddings = SinusoidalEmbeddings(ts)
 
         xt, eps, _ = self.noise_schedule.make_noisy_images(x0.flatten(1), ts)
         xt = xt.view(batch_size, *self.dimensions)
 
         xt = xt.to(self.device)
-        embeddings = embeddings.to(self.device)
         eps = eps.to(self.device)
+        ts = ts.to(self.device)
 
-        pred = self.network(xt, embeddings)
+        pred = self(xt, ts.flatten())
         loss = mse(pred.flatten(1), eps)
         return loss
     
-    def get_prior(self, xt, embedding, t):
-        eps = self.network(xt.view(1, *self.dimensions), embedding).flatten()
+    def get_prior(self, xt, t):
+        t_for_model = torch.tensor([t]).to(self.device)
+        eps = self(xt.view(1, *self.dimensions), t_for_model).flatten()
 
         k1 = 1 / self.noise_schedule.sqrt_alpha[t]
         k2 = (1 - self.noise_schedule.alpha[t]) / self.noise_schedule.sqrt_one_minus_alpha_hat[t]
@@ -302,37 +227,46 @@ class SimpleModel(Model):
         
         return xt
     
+    def forward(self, x, t):
+        return self.network(x, t)
+    
 
-class MuModel(Model):
+class x0Model(Model):
     def __init__(self, network, noise_schedule, dimensions, device):
         super().__init__(network, noise_schedule, dimensions, device)
-        self.path = "../models/mu_model.pt"
+        self.path = "../models/x0_model.pt"
     
-    def loss(self, x0):
+    def loss(self, x0, ts= None):
         batch_size = len(x0)
-        ts = self.noise_schedule.sample_time_steps(batch_size)
-        embeddings = SinusoidalEmbeddings(ts)
+        ts = self.noise_schedule.sample_time_steps(batch_size) if ts is None else ts
+
         xt, _, _ = self.noise_schedule.make_noisy_images(x0.flatten(1), ts)
         xt = xt.view(batch_size, *self.dimensions)
 
         xt = xt.to(self.device)
-        embeddings = embeddings.to(self.device)
         x0 = x0.to(self.device)
+        ts = ts.to(self.device)
 
-        pred = self.network(xt, embeddings)
+        pred = self(xt, ts.flatten())
         loss = mse(pred.flatten(1), x0.flatten(1))
         return loss
     
-    def get_prior(self, xt, embedding, t):
-        x0 = self.network(xt.view(1, *self.dimensions), embedding).flatten(1)
+    def get_prior(self, xt, t):
+        t_for_model = torch.tensor([t]).to(self.device)
+        x0 = self(xt.view(1, *self.dimensions), t_for_model).flatten()
+
+        if t == 0:
+            return x0
+        
         k1 = self.noise_schedule.sqrt_alpha[t] * (1 - self.noise_schedule.alpha_hat[t - 1]) * xt
         k2 = self.noise_schedule.sqrt_alpha_hat[t - 1] * (1 - self.noise_schedule.alpha[t]) * x0
         xt = (k1 + k2) / (1 - self.noise_schedule.alpha_hat[t])
-
-        if t > 0:
-            xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
+        xt += self.noise_schedule.sigma[t].to(self.device) * torch.randn((1, self.img_size)).to(self.device)
 
         return xt
+
+    def forward(self, x, t):
+        return torch.tanh(self.network(x, t))
     
 class ELBOModel(Model):
     def __init__(self, network, noise_schedule, dimensions, device):
@@ -385,21 +319,3 @@ class ELBOModel(Model):
 
         if t > 0:
             xt += self.noise_schedule.beta[t] * torch.randn((1, self.img_size)).to(self.device)
-
-if __name__ == "__main__":
-    """ n = 4
-    noise_schedule = NoiseSchedule(1e-4, 0.02, 1000)
-    dimensions = [1, 28, 28]
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    network = ResNET(channels=dimensions[0])
-    model = SimpleModel(network, noise_schedule, dimensions, device)
-    dummy_input = torch.randn(n, *dimensions).to(device)
-    dummy_time = noise_schedule.sample_time_steps(n)
-    dummy_embedding = SinusoidalEmbeddings(dummy_time).to(device)
-    dummy_out = model.network(dummy_input, dummy_embedding)
-    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print("Number of parameters: {}".format(num_params))
-    print(dummy_out.shape)
-    xt = model.sample() """
-
-    data = CelebA()
